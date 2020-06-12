c669aa7b48543f49120f1718ceb8eba18bedcf4c
TomAbel <tomkristian.abel@tptlive.ee>
2020-06-12 19:06:21 +0300

    include build folder

---
+++ build/precache-manifest.702501ea5f9fcc22d20168b864a8167c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947f57b231156a6019c5cc8519bcd793",
+    "url": "/reactcardgame/index.html"
+  },
+  {
+    "revision": "09d6722a441eedccbb0a",
+    "url": "/reactcardgame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "289a1ddcd324cf98e0dc",
+    "url": "/reactcardgame/static/js/2.fb2272af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactcardgame/static/js/2.fb2272af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d6722a441eedccbb0a",
+    "url": "/reactcardgame/static/js/main.d76cde02.chunk.js"
+  },
+  {
+    "revision": "fd65312627faff3c4fa2",
+    "url": "/reactcardgame/static/js/runtime-main.17eef323.js"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reactcardgame/precache-manifest.702501ea5f9fcc22d20168b864a8167c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactcardgame/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
